f82a774fd37b9d98f890b14918bf5c547cae1c4f
YorguinL <56040482+YorguinL@users.noreply.github.com>
2020-02-05 19:29:00 +0100

    Update server.py

--- Practica16_YorguinLayton/server.py
+++ Practica16_YorguinLayton/server.py
@@ -7,11 +7,12 @@ HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
+s.listen(1)
 conn, addr = s.accept()
 
 t_env = Thread(target=missatge.enviar_missatge, args=(conn,))
 t_reb = Thread(target=missatge.rebre_missatge, args=(conn,) )
-t_env.deamon = True
+t_env.daemon = True
 
 t_env.start()
 t_reb.start()
